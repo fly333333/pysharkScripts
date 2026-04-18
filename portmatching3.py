f7b37768badbe34ca08475fe9343657cdf59425d
fly333333 <matt.ryn.martin@gmail.com>
2026-04-18 14:59:18 -0700

    testing stuff

---
+++ portmatching3.py
@@ -0,0 +1,56 @@
+import sys
+import pyshark
+
+# Citation: Used Scott Kirlin's "listProtocols.py" for starters.
+# Adding to "insecureProtocols.py" script for this one.
+
+class NetworkProtocol:
+    def __init__(self, name, default_port):
+        self.name = name
+        self.default_port = str(default_port)
+
+    def port_match(self, packet):
+        if hasattr(packet, 'tcp'):
+            if packet.tcp.srcport == self.default_port or packet.tcp.dstport == self.default_port:
+                return True
+        return False
+
+protocolList = [
+    NetworkProtocol("HTTP", 80),
+    NetworkProtocol("TELNET", 23),
+    NetworkProtocol("SSH", 22),
+    NetworkProtocol("TLS", 443)
+]
+
+def main():
+    fileName = sys.argv[1]
+    capture = pyshark.FileCapture(fileName)
+    for packet in capture:
+        if not hasattr(packet, 'ip'):
+            continue
+
+        # Took me forever to figure out: filter out the TCP handshake packets
+        tcp_payload_len = int(getattr(packet.tcp, 'len', 0))
+        if tcp_payload_len == 0:
+            continue
+
+        packet_layers = [layer.layer_name.upper() for layer in packet.layers]
+        print(f"Packet {packet.number} has non upper layers: {packet.layers}")
+        print(f"Packet {packet.number} has upper layers: {packet_layers}")
+        dest_port = "N/A"
+        if hasattr(packet, 'tcp'):
+            dest_port = packet.tcp.dstport
+        elif hasattr(packet, 'udp'):
+            dest_port = packet.udp.dstport
+
+        for knownProtocol in protocolList:
+            if knownProtocol.port_match(packet):
+                if knownProtocol.name not in packet_layers:
+                    print(f"Wrong Service On Port:")
+                    print(f"    Packet {packet.number}: Port {dest_port} is reserved for {knownProtocol.name},")
+                    print(f"    but the packet contains: {packet.layers}")
+                    print(f"    Connection: {packet.ip.src} -> {packet.ip.dst}\n")
+    capture.close()
+
+if __name__ == "__main__":
+    main()
