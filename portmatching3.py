b73cdb61114f61719c66273be936b4dcc8fb57bd
fly333333 <matt.ryn.martin@gmail.com>
2026-04-18 15:37:15 -0700

    clean-up, moved prev portMatching versions to recycling bin

--- portmatching3.py
+++ portmatching3.py
@@ -1,54 +1,53 @@
 import sys
 import pyshark
 
-# Citation: Used Scott Kirlin's "listProtocols.py" for starters.
-# Adding to "insecureProtocols.py" script for this one.
-
 class NetworkProtocol:
     def __init__(self, name, default_port):
         self.name = name
         self.default_port = str(default_port)
-
-    def port_match(self, packet):
-        if hasattr(packet, 'tcp'):
-            if packet.tcp.srcport == self.default_port or packet.tcp.dstport == self.default_port:
-                return True
-        return False
 
 protocolList = [
     NetworkProtocol("HTTP", 80),
     NetworkProtocol("TELNET", 23),
     NetworkProtocol("SSH", 22),
     NetworkProtocol("TLS", 443)
+    NetworkProtocol("DNS", 53)
+
 ]
 
 def main():
     fileName = sys.argv[1]
     capture = pyshark.FileCapture(fileName)
     for packet in capture:
+
+        # Not an IP Packet, skip.
         if not hasattr(packet, 'ip'):
             continue
 
+        # Get the Layers within the Packet.
         packet_layers = [layer.layer_name.upper() for layer in packet.layers]
-        # print(f"Packet {packet.number} has non upper layers: {packet.layers}")
-        # print(f"Packet {packet.number} has upper layers: {packet_layers}")
+
+        # Set udp/tcp destination port.
         dest_port = "N/A"
         if hasattr(packet, 'tcp'):
             dest_port = packet.tcp.dstport
         elif hasattr(packet, 'udp'):
             dest_port = packet.udp.dstport
 
+        # Core check for mismatched ports/services.
         for knownProtocol in protocolList:
             if dest_port == knownProtocol.default_port:
                 if knownProtocol.name not in packet_layers:
                     print(f"Wrong Service On Port:")
-                    print(f"    Packet {packet.number}: Port {dest_port} is reserved for {knownProtocol.name},")
+                    print(f"    Packet {packet.number}: Port {dest_port} is for {knownProtocol.name},")
                     print(f"    but the packet contains: {packet_layers}")
-                    print(f"    Connection: {packet.ip.src} -> {packet.ip.dst}\n")
+                    print(f"    Connection: {packet.ip.src} -> {packet.ip.dst}")
 
+                # Check if the packet has a TCP length of 0. If so, this means this packet is a handshake, a scan (i.e. RST flag), or empty.
+                # I previously had this above the check, however, its more valuable to mention after the check, rather than skip the check if it a Handshake.
                 tcp_payload_len = int(getattr(packet.tcp, 'len', 0))
                 if tcp_payload_len == 0:
-                    print(f"Packet {packet.number} with Port {dest_port} has TCP length 0. Potential Handshake, Scanning or otherwise empty TCP packet.")
+                    print(f"Packet {packet.number} with Port {dest_port} has TCP length 0. Potential Handshake, Scanning or otherwise empty TCP packet.\n")
 
     capture.close()
 
